cfcc2803c9388a174fc236bf8cc0f35da978e28c
dmoreir1 <dmoreir1@binghamton.edu>
2023-08-31 15:15:35 -0400

    Testing file upload function

---
+++ file_upload.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, redirect
+import pandas as pd
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and file.filename.endswith('.xlsx'):
+            df = pd.read_excel(file)
+            print(df.head())
+            return redirect('/')
+    return '''
+    <!doctype html>
+    <title>Upload an Excel File</title>
+    <h1>Upload an Excel file</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+if __name__ == '__main__':
+    app.run(debug=True)
